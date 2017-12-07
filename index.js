cabae4047eb2c75ef6e0f8d515f423aa6477be5d
Andreas Holstenson <a@holstenson.se>
2017-12-07 17:26:39 +0100

    Adding support for voltages

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ const power = require('./lib/power');
 const pressure = require('./lib/pressure');
 const speed = require('./lib/speed');
 const temperature = require('./lib/temperature');
+const voltage = require('./lib/voltage');
 const volume = require('./lib/volume');
 
 module.exports = {
 	amount, generic: amount,
 	area, angle, duration, energy, illuminance, length, mass, power, pressure,
-	speed, temperature, volume
+	speed, temperature, voltage, volume
 };
